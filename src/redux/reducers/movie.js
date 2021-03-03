f3d0d5138c9ec490bfd781b89f4b515eccd0b865
David Bieder <david@cultivate.team>
2021-03-03 11:25:30 +0100

    Simplify reducer

--- src/redux/reducers/movie.js
+++ src/redux/reducers/movie.js
@@ -2,11 +2,11 @@ import { MOVIE_CREATE } from '../actions/movie'
 
 export const initialState = ''
 
-export const reducer = (movie = initialState, action) => {
+export const reducer = (movie, action) => {
   switch (action.type) {
   case MOVIE_CREATE:
     return action.movie
   default:
-    return movie
+    return initialState
   }
 }
